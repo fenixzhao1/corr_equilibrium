394920b7d3cec7bb403200514db34629ef3ff81e
Shuchen Zhao <31258178+fenixzhao1@users.noreply.github.com>
2021-06-05 22:46:35 -0700

    update pair level simulation analysis

--- simulations/MVrunhm2000.R
+++ simulations/MVrunhm2000.R
@@ -14,7 +14,7 @@ mu = 1000 # HM2000 probability parameter
 n = 500 # number of periods in each simulation
 sim = 500 # number of simulations
 experiment = 100 # number of experimentation periods where players randomly make decisions
-beta = c(0.99,-0.24)
+beta = c(0.95,-0.14)
 Delta = 0.98
 pay_MV = matrix(c(0,200,100,100,0,200,200,100,0),3,3) # payoff matrix 3x3
 
@@ -25,12 +25,10 @@ joint_density_all = matrix(c(0,0,0,0,0,0,0,0,0),3,3)
 joint_density = list()
 
 # set up the overall dataset
-df_all = data.frame(
-  p1_choice = NA,
-  p2_choice = NA,
-  period = NA,
-  sim_id = NA)
-df_all = filter(df_all, is.na(p1_choice)==FALSE)
+length = rep(0, sim)
+df_sim = data.frame(sim = length, jd_11 = length, jd_12 = length, jd_13 = length, jd_21 = length,
+                    jd_22 = length, jd_23 = length, jd_31 = length, jd_32 = length, jd_33 = length,
+                    p1_payoff = length, p2_payoff = length)
 
 # run the simulations
 for (s in 1:sim){
@@ -48,8 +46,8 @@ for (s in 1:sim){
   
   # calculate the rest of the decisions to n periods
   for (i in (experiment+1):n){
-    history_p1[i] = decision_avgpay_logit_truncate(mu, beta, i, history_p1, history_p2)
-    history_p2[i] = decision_avgpay_logit_truncate(mu, beta, i, history_p2, history_p1)
+    history_p1[i] = decision_avgpay_InertiaLogit_truncate(mu, beta, Delta, i, history_p1, history_p2)
+    history_p2[i] = decision_avgpay_InertiaLogit_truncate(mu, beta, Delta, i, history_p2, history_p1)
     
     # update the joint density matrix
     if (history_p1[i]==1 & history_p2[i]==1){joint_density[[s]][1,1]=joint_density[[s]][1,1]+1}
@@ -66,15 +64,41 @@ for (s in 1:sim){
   # normalize the frequency to probability
   joint_density[[s]] = round(joint_density[[s]]/sum(joint_density[[s]]), 3)
   
-  # create the dataset
+  # create the dataset for this simulation
   df = data.frame(
-    p1_choice = history_p1,
-    p2_choice = history_p2,
-    period = 1:n,
-    sim_id = rep(s, n)
-  )
+    p1_choice = history_p1[(experiment+1):n],
+    p2_choice = history_p2[(experiment+1):n],
+    period = (experiment+1):n)
+  df = df %>% mutate(
+    is_11 = ifelse(p1_choice==1 & p2_choice==1, 1, 0),
+    is_12 = ifelse(p1_choice==1 & p2_choice==2, 1, 0),
+    is_13 = ifelse(p1_choice==1 & p2_choice==3, 1, 0),
+    is_21 = ifelse(p1_choice==2 & p2_choice==1, 1, 0),
+    is_22 = ifelse(p1_choice==2 & p2_choice==2, 1, 0),
+    is_23 = ifelse(p1_choice==2 & p2_choice==3, 1, 0),
+    is_31 = ifelse(p1_choice==3 & p2_choice==1, 1, 0),
+    is_32 = ifelse(p1_choice==3 & p2_choice==2, 1, 0),
+    is_33 = ifelse(p1_choice==3 & p2_choice==3, 1, 0))
+  for (j in 1:length(df$period)){
+    df$p1_payoff[j] = pay_MV[df$p1_choice[j], df$p2_choice[j]]
+    df$p2_payoff[j] = pay_MV[df$p2_choice[j], df$p1_choice[j]]
+  }
   
-  df_all = rbind(df_all, df)
+  # record the sim ID to the overall data
+  df_sim$sim[s] = s
+  
+  # record the joint density
+  df_sim$jd_11[s] = mean(df$is_11)
+  df_sim$jd_12[s] = mean(df$is_12)
+  df_sim$jd_13[s] = mean(df$is_13)
+  df_sim$jd_21[s] = mean(df$is_21)
+  df_sim$jd_22[s] = mean(df$is_22)
+  df_sim$jd_23[s] = mean(df$is_23)
+  df_sim$jd_31[s] = mean(df$is_31)
+  df_sim$jd_32[s] = mean(df$is_32)
+  df_sim$jd_33[s] = mean(df$is_33)
+  df_sim$p1_payoff[s] = mean(df$p1_payoff)
+  df_sim$p2_payoff[s] = mean(df$p2_payoff)
   
   # # graph the decision making
   # title = paste('hm2000r', 'MV', 'sim', as.character(s), sep = '_')
@@ -104,6 +128,27 @@ for (a in 1:sim){
 }
 joint_density_all = joint_density_all / sim
 xtable(joint_density_all)
+
+# calculate the fraction in CE
+df_sim$in_ce <- 0
+df_sim <- df_sim %>%
+  mutate(in_ce = ifelse(p1_payoff>100 & abs((p2_payoff-100)/(p1_payoff-100))>=1/2 & abs((p2_payoff-100)/(p1_payoff-100))<=2,1,in_ce))
+df_sim<- df_sim %>%
+  mutate(in_ce = ifelse(p1_payoff==100 & p2_payoff==100,1,in_ce))
+
+# calculate the distance
+df_sim = df_sim %>%
+  mutate(
+    d_mne = sqrt((p1_payoff-100)^2+(p2_payoff-100)^2),
+    d_tce = sqrt((p1_payoff-150)^2+(p2_payoff-150)^2))
+
+# display the results
+mean(df_sim$in_ce)
+mean(df_sim$d_mne)
+mean(df_sim$d_tce)
+t.test(df_sim$d_mne, df_sim$d_tce, mu=0, paired = TRUE)$p.value
+
+rm(df, df_sim, joint_density, joint_density_all, history_p1, history_p2)
 
 
 ##### Transition between cells #####
